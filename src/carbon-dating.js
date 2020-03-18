cf7fe19a9130256f6fcecf6a72a933fefa45fb48
Yury <chifius@mail.ru>
2020-03-19 02:52:10 +0300

    feat: finish carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,12 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
+module.exports = function dateSample(sampleActivity) {
+  //throw 'Not implemented';
   // remove line with error and write your code here
+  return ((typeof (sampleActivity) !== 'string') || 
+          (isNaN(parseFloat(sampleActivity))) ||
+          (sampleActivity <= 0) || (sampleActivity > 15)) ?
+      false :        
+      Math.ceil(Math.log(MODERN_ACTIVITY / parseFloat(sampleActivity)) / (0.693 / HALF_LIFE_PERIOD));
 };
