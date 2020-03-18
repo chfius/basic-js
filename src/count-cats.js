75a31e0b28c441325c89ea09276dd193e28489bc
Yury <chifius@mail.ru>
2020-03-19 00:51:10 +0300

    feat: finish count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,21 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
+module.exports = function countCats(n/* matrix */) {  
+  //throw 'Not implemented';
   // remove line with error and write your code here
+  let count = 0;
+  function arrayResizer(e) {
+      if ((typeof (e) === 'object') && (e !== null)) {
+          e.forEach(elem => {
+              arrayResizer(elem);
+          });
+      } else {
+          if (e === '^^') { count++ };
+          return e;
+      }
+  }
+  if (n.length !== 0) {
+      n.forEach(element => {
+          arrayResizer(element);
+      });
+  }
+  return count;
 };
