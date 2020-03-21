63bce4a86ef68c56ab50e8a63403a1b0937b414d
Yury <chifius@mail.ru>
2020-03-22 01:57:24 +0300

    feat: finish Dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,20 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+    // throw 'Not implemented';
+    // remove line with error and write your code here
+
+    let str = [];
+
+    if (Array.isArray(members)) {
+        members.forEach(element => {
+            if (typeof(element) == "string") {
+                for (let i = 0; i < element.length; i++) {
+                    if (element[i] !== ' ') {
+                        str.push(element[i].toUpperCase());
+                        break;
+                    }
+                }
+            }
+        });
+    }
+    return (str.length !== 0) ? str.sort().join('') : false;
 };
